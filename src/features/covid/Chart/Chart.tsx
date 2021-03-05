b6a20e8ad57bf2f45243e45f6ba799398c8798f8
shunak <shuna966@gmail.com>
2021-03-05 16:05:08 +0900

    modified

--- src/features/covid/Chart/Chart.tsx
+++ src/features/covid/Chart/Chart.tsx
@@ -3,7 +3,6 @@ import styles from "./Chart.module.css";
 import { Line } from "react-chartjs-2";
 import { useSelector } from "react-redux";
 import { selectDaily } from "../covidSlice";
-
 const Chart: React.FC = () => {
   const daily = useSelector(selectDaily);
   const dates = daily.map(({ Date }) => Date);
